e8be45dce6f4301e9ab84362c9da61e58d9212b6
Andrrr <andresin@gmail.com>
2018-02-07 19:01:17 -0300

    updated db conn selection

--- db.js
+++ db.js
@@ -3,19 +3,20 @@
 var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
-var mongo_user = process.env.OPENSHIFT_MONGODB_DB_USERNAME || process.env.MONGO_USER;
-var mongo_db = process.env.OPENSHIFT_APP_NAME || process.env.MONGO_DB;
+var mongo_user = process.env.MONGO_USER || process.env.OPENSHIFT_MONGODB_DB_USERNAME;
+var mongo_db = process.env.MONGO_DB || process.env.OPENSHIFT_APP_NAME;
 
 if (mongo_user == process.env.MONGO_USER) {
-    var MongoDB = mongoose.connect('mongodb://localhost/velours').connection;
+    // var MongoDB = mongoose.connect('mongodb://localhost/velours').connection;
+    var MongoDB = mongoose.connect(process.env.MONGO_HOST + mongo_db).connection;
 } else {
     var MongoDB = mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL + mongo_db).connection;
 }
 
-MongoDB.on('error', function(err) {
+MongoDB.on('error', function (err) {
     console.log(err.message);
 });
-MongoDB.once('open', function() {
+MongoDB.once('open', function () {
     console.log('Connected to MongoDB with user ' + mongo_user);
 });
 
@@ -45,8 +46,8 @@ var CanevaSchema = new mongoose.Schema({
 });
 
 
-var Config  = mongoose.model('condfig', ConfigSchema);
-var Caneva  = mongoose.model('caneva', CanevaSchema);
+var Config = mongoose.model('config', ConfigSchema);
+var Caneva = mongoose.model('caneva', CanevaSchema);
 
 var schemas = {
     'Config': Config,
